7bb1037252efec625a4d96ba15c1828bf66e5931
MatthewLyons1056 <39568363+MatthewLyons1056@users.noreply.github.com>
2020-04-05 20:59:58 -0400

    Matts Train Build
    - - -

---
+++ 3D Train game/Assets/Scripts/MoveCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f * Time.deltaTime * speed, 5f * Time.deltaTime * speed, 0f * Time.deltaTime);
+    }
+}
---
+++ 3D Train game/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float XDirection = 0;
+    public float YDirection = 15;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement section
+
+        if (Input.GetKey("a"))
+        {
+            print("Left");
+            YDirection = 0;
+            XDirection = -15;
+            
+
+            //transform.Translate(XDirection * Time.deltaTime * speed, 0f, 0f);
+        }
+        if (Input.GetKey("w"))
+        {
+            print("Up");
+
+            YDirection = 15;
+            XDirection = 0;
+
+            //transform.Translate(0f, 0f, YDirection * Time.deltaTime * speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            print("Right");
+
+            YDirection = 0;
+            XDirection = 15;
+
+            //transform.Translate(XDirection * Time.deltaTime * speed, 0f, 0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            print("Down");
+
+            YDirection = -15;
+            XDirection = 0;
+
+            //transform.Translate(0f, 0f, -15f * Time.deltaTime * speed);
+        }
+        transform.Translate(XDirection * Time.deltaTime * speed, 0f, YDirection * Time.deltaTime * speed);
+
+
+
+        //transform.Translate(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, 0f, Input.GetAxisRaw("Vertical") * Time.deltaTime * speed);
+    }
+}

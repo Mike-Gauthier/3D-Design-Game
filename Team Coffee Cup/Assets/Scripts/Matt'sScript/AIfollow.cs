1c063a50ffe693c0d45817d33f9d50703da6a836
MatthewLyons1056 <39568363+MatthewLyons1056@users.noreply.github.com>
2020-04-13 13:45:26 -0400

    added stuff lol
    lol

--- Team Coffee Cup/Assets/Scripts/Matt'sScript/AIfollow.cs
+++ Team Coffee Cup/Assets/Scripts/Matt'sScript/AIfollow.cs
@@ -19,7 +19,7 @@ public class AIfollow : MonoBehaviour
 
     ///public texts
 
-    public Text RailNumber;
+    public Text RailTypeText;
 
 
     //list of rails
@@ -28,8 +28,11 @@ public class AIfollow : MonoBehaviour
     public GameObject railleft;
     public GameObject railright;
 
+    public GameObject CurrentRail;
+
+
     //Change rail
-    public int Swaprail;
+    public int Swaprail = 1;
     
     public LinkedList<GameObject> myLinkedList;
     //////
@@ -47,6 +50,7 @@ public class AIfollow : MonoBehaviour
         
         
         print(myLinkedList.Count);
+        
     }
     void Update()
 
@@ -55,11 +59,44 @@ public class AIfollow : MonoBehaviour
 
     { //@TODO }
 
+        
+
+        if (Input.GetKeyDown("q"))
+        {
+            
+            print("q");
+            Swaprail = 0;
+            //changes rail to 0 aka left turn
+            CurrentRail = railleft;
+            RailTypeText.text = "Rail in use: LEFT RAIL";
+            print(RailTypeText);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            print("W");
+            Swaprail = 1;
+            //Changes rail to 1, aka straight rail
+            CurrentRail = railstraight;
+            RailTypeText.text = "Rail in use: STRAIGHT RAIL";
+            print(RailTypeText);
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            print("E");
+            Swaprail = 2;
+            //Changes rail to 2, aka right turn
+            CurrentRail = railright;
+            RailTypeText.text = "Rail in use: RIGHT RAIL";
+            print(RailTypeText);
+        }
 
         //button controls - to control which rail is being placed
 
+      
 
-        if(myLinkedList.Count !=0)
+        if (myLinkedList.Count !=0)
         {
             target = myLinkedList.Last.Value.transform;
         }
@@ -84,7 +121,7 @@ public class AIfollow : MonoBehaviour
                 newPosition = OldPosition + new Vector3(0, 0, 1); //Controls location of new rail. 
             }
 
-            actualRail = Instantiate(railstraight, newPosition, railstraight.transform.rotation);
+            actualRail = Instantiate(CurrentRail, newPosition, CurrentRail.transform.rotation);
             myLinkedList.AddLast(actualRail.transform.GetChild(0).gameObject);
             print(myLinkedList.Count);
         }

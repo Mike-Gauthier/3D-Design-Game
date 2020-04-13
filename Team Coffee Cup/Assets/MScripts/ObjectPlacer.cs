f56eaf41a08d47c210f29898a58a1a3bb932560a
Mike Gauthier <mgauth12@student.fitchburgstate.edu>
2020-04-12 21:06:45 -0400

    Mike Changes 4/12

---
+++ Team Coffee Cup/Assets/MScripts/ObjectPlacer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public float timer;
+    public float chance;
+    public GameObject obstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            chance = Random.Range(1, 10);
+            if(chance < 5)
+            {
+                Instantiate(obstacles, transform.position, transform.rotation);
+            }
+            else { }
+            timer = 10.0f;
+        }
+
+    }
+}

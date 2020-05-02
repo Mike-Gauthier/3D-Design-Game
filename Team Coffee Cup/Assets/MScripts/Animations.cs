c6dff13843ad6637f8a3f278307df769d0205b47
Mike Gauthier <mgauth12@student.fitchburgstate.edu>
2020-05-02 00:54:40 -0400

    Animations

--- Team Coffee Cup/Assets/MScripts/Animations.cs
+++ Team Coffee Cup/Assets/MScripts/Animations.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Animations : MonoBehaviour
 {
+    //have Matt call these variables and set them to true on instantiation
+    Animator anim;
+    bool isTurnLeft, isTurnRight, isCrashing = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isTurnLeft && isTurnRight && isCrashing != true)
+        {
+            anim.SetBool("TrainIdle", true);
+        }
+        else
+        {
+            anim.SetBool("TrainIdle", false);
+        }
+
+        if(isTurnLeft == true)
+        {
+            anim.SetBool("TrainLeft", true);
+        }
+        else
+        {
+            anim.SetBool("TrainLeft", false);
+        }
+
+        if (isTurnRight == true)
+        {
+            anim.SetBool("TrainRight", true);
+        }
+        else
+        {
+            anim.SetBool("TrainRight", false);
+        }
+
+        if (isCrashing == true)
+        {
+            anim.SetBool("TrainCrash", true);
+        }
+        else
+        {
+            anim.SetBool("TrainCrash", false);
+        }
     }
 }

66a9d9eb11014aa276ca0978db1c2d28fb0dbb4e
arlocke <alocke692@gmail.com>
2020-05-07 18:20:51 -0400

    Archway Bell ringing added

---
+++ Team Coffee Cup/Assets/Scripts/AlexScripts/BellRing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class BellRing : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    AudioSource bellRing;
+
+    void Start()
+    {
+        bellRing = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Train Collide");
+            myAnimationController.SetBool("playRing", true);
+            bellRing.Play();
+        }
+    }
+
+    /*
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("playRing", false);
+        }
+    }*/
+}

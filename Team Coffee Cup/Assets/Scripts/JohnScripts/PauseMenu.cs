288917d07fd7c71a436d9dd8dc95846e24529207
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2020-05-07 20:10:35 -0400

    Implemented Pause Menu

--- Team Coffee Cup/Assets/Scripts/JohnScripts/PauseMenu.cs
+++ Team Coffee Cup/Assets/Scripts/JohnScripts/PauseMenu.cs
@@ -43,6 +43,7 @@ public class PauseMenu : MonoBehaviour
     }
     public void ReturnToMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(mainMenu);
     }
     public void QuitGame()

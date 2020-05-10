797f41ec092f6b579503c3a60060890be7b4ee5d
arlocke <alocke692@gmail.com>
2020-05-09 22:18:27 -0400

    Added crash script

---
+++ Team Coffee Cup/Assets/Scripts/AlexScripts/ObstacleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    public GameObject player;
+
+    AudioSource crashSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        crashSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Train Crash");
+            crashSound.Play();
+            player.GetComponent<AIfollow>().enabled = false;
+        }
+    }
+}

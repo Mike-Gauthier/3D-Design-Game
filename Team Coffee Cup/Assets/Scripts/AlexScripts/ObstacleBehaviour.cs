e09bf62bf53c2d80523cbcffdbc594d7c95c0e21
arlocke <alocke692@gmail.com>
2020-05-10 01:35:10 -0400

    colliders on everything

--- Team Coffee Cup/Assets/Scripts/AlexScripts/ObstacleBehaviour.cs
+++ Team Coffee Cup/Assets/Scripts/AlexScripts/ObstacleBehaviour.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObstacleBehaviour : MonoBehaviour
 {
     public GameObject player;
 
+    public GameObject gameOverFrame;
+
     AudioSource crashSound;
+
+    public bool hasCrashed;
+    public string mainMenu;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         crashSound = GetComponent<AudioSource>();
+        gameOverFrame = GameObject.FindGameObjectWithTag("GameOver");
+        gameOverFrame.transform.localScale = new Vector3(0, 0, 0);
+        hasCrashed = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,8 +29,11 @@ public class ObstacleBehaviour : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Train Crash");
+            hasCrashed = true;
             crashSound.Play();
-            player.GetComponent<AIfollow>().enabled = false;
+            player.GetComponent<AIfollow>().enabled = false; //Removes AI follow script for now, can be replaced for better optimization
+            //Put crash animation here
+            gameOverFrame.transform.localScale = new Vector3(5, 5, 5);
         }
     }
 }
